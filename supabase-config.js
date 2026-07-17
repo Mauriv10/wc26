91332393c0417df6472086070171170720144ddd
Mauriv10 <mauri.villalobos92@gmail.com>
2026-07-17 16:50:20 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Pega la Publishable key ENTRE COMILLAS. Nunca uses aquí una Secret key/service_role.
 window.WC_SUPABASE_CONFIG = {
   url: "https://xkjtiwbsxsvyrsqtaqgi.supabase.co",
-  publishableKey: "PEGA_AQUI_TU_SB_PUBLISHABLE_KEY"
+  publishableKey: "sb_publishable_7hUkQzyNiObLuo3yx4RlMg_O-IBpRTL"
 };
